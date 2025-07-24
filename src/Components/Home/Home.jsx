5bedfd253c43e5d7e756b07b1804526c5899359f
BassemAl-Abyad <b.yossry@gmail.com>
2025-07-24 20:10:10 +0300

    added modules.css part for home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import mainImage from '/src/assets/main.png';
+import styles from './Home.module.css'
 
 export default function Home() {
   return (
-    <div className="bg-content text-white p-5">
+    <div className={`${styles.bg__content} text-white p-5`}>
       <div className="container">
         <div className="d-flex flex-column align-items-center justify-content-center p-2">
           <img
@@ -14,15 +15,15 @@ export default function Home() {
           />
         </div>
         <div className="text-center p-2">
-          <h1 className="text-uppercase header-font fw-bold">
+          <h1 className={`${styles.header__font} text-uppercase fw-bold`}>
             Start Framework
           </h1>
         </div>
 
         <div className="d-flex justify-content-center align-items-center p-2">
-          <div className="line bg-white mx-3"></div>
+          <div className={`${styles.line__art} bg-white mx-3`}></div>
           <FontAwesomeIcon icon={faStar} />
-          <div className="line bg-white mx-3"></div>
+          <div className={`${styles.line__art} bg-white mx-3`}></div>
         </div>
         <div className="text-center p-2">
           <p>Graphic Artist - Web Designer - Illustrator</p>
